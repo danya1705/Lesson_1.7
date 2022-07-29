d9e15592e85e632877f7541335c16c2bb56a9f59
danya1705 <danya17@gmail.com>
2022-07-29 13:37:10 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Задание 1
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+
+        //Задание 2
+
+        String fullNameInUpperCase = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameInUpperCase);
+
+
+        //Задание 3
+
+        fullName = "Иванов Семён Семёнович";
+        String fullNameWithoutYo = fullName.replace('ё', 'е');
+        fullNameWithoutYo = fullNameWithoutYo.replace('Ё', 'Е');
+        System.out.println("Данные ФИО сотрудника - " + fullNameWithoutYo);
 
     }
 }
